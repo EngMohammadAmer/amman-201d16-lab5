be62fb20e27b5f478a47c98bf608ef3d6536fb87
Mohammad Amer <msadeq_eng@yahoo.com>
2021-02-14 03:25:07 +0200

    problem 4 solved

--- app.js
+++ app.js
@@ -102,7 +102,6 @@ function sumArray(sumArr) {
 }
 console.log(sumArray(testArray));
 // Here is the test for sumArray(); uncomment it to run it
-
 testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
